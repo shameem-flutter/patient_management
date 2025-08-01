d826997c9d99f409a4c3422ae866eba505c8fdff
shameem-flutter <shameem.m.a999@gmail.com>
2025-08-01 19:14:49 +0530

    feat: add ApiService for login, patient list and patient registration

---
+++ lib/services/api_service.dart
@@ -0,0 +1,46 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static String baseUrl = "https://flutter-amr.noviindus.in/api";
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("token");
+  }
+
+  Future<http.Response> login(String username, String password) async {
+    final url = Uri.parse("$baseUrl/Login");
+    return await http.post(
+      url,
+      body: {"username": username, "password": password},
+    );
+  }
+
+  Future<http.Response> getPatientList() async {
+    final token = await getToken();
+    final url = Uri.parse("$baseUrl/PatientList");
+    return await http.get(url, headers: {"Authorization": "Bearer $token"});
+  }
+
+  Future<http.Response> getBranches() async {
+    final token = await getToken();
+    final url = Uri.parse("$baseUrl/BrancheList");
+    return await http.get(url, headers: {"Authorization": "Bearer $token"});
+  }
+
+  Future<http.Response> getTreatmentList() async {
+    final token = await getToken();
+    final url = Uri.parse("$baseUrl/TreatmentList");
+    return await http.get(url, headers: {"Authorisation": "Bearer $token"});
+  }
+
+  Future<http.Response> registerPatient(Map<String, dynamic> data) async {
+    final token = await getToken();
+    final url = Uri.parse("$baseUrl/PatientUpdate");
+    return await http.post(
+      url,
+      headers: {"Authorization": "Bearer $token"},
+      body: token,
+    );
+  }
+}
